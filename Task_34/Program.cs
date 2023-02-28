b0c6b4a675146099ad033b72a5e4fe5b286937fa
nikita <tolstoguzov.nikita98@mail.ru>
2023-02-28 15:26:08 +0300

    Задача 34

---
+++ Task_34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    Random rnd = new Random();
+    for(int i = 0; i < size; i++)
+    result[i] = rnd.Next(100, 1000);
+    Console.WriteLine($"Ваш массив: [{string.Join(", ", result)}]");
+    return result;
+}
+
+void GetEvenNum (int[] array)
+{
+    int count = 0;
+    foreach(int elem in array)
+    {
+        if(elem % 2 == 0)
+        count ++;
+    }
+    Console.WriteLine($"Количество четных элементов в массиве: {count}");
+}
+
+int[] arr = GetArray(12);
+GetEvenNum(arr);
